fafd7911c54277eba07888b896fb96c0ef6e4eea
paverz <92654842+paverz@users.noreply.github.com>
2021-10-17 22:11:09 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,191 @@
+
+/*
+
+//Lecture 1
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+// Output in Console within browser
+console.log('Jonas');
+console.log('23');
+// Define a Value
+let firstName = "Matilda";
+
+// Variables for output
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas'
+
+//VS code knows about PI hence it is marked in purple
+let PI = 3.1415;
+
+let myFirstJob = 'Network Engineer';
+let myCurrentJob = 'Support Agent';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+//////////////////////////////////////////////////////
+
+//Assignment Part 1 Lecture: Values and Variables
+let Country = 'Australia';
+let Continent = 'Oceanic Region';
+let Population = '25.69 Million'
+
+console.log(Country);
+console.log(Continent);
+console.log(Population);
+
+//////////////////////////////////////////////////////
+
+
+/** Lecture 2 - Data Types
+// Boolean
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+//////////////////////////////////////////////////////
+
+// Assignment Data Types
+true;
+let isIsland
+let population
+let country
+let language
+
+
+isIsland = 'Australia';
+population = '25.69 million';
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+//////////////////////////////////////////////////////
+
+// let, const and var //
+let age = 30;
+age = 31;
+
+const birthYear = 1994;
+// birthYear = 1990;
+
+var job = 'Network Engineer';
+job = 'teacher'
+
+lastName = 'Prakash';
+console.log(lastName);
+
+///////////////////////////////////////////////////////
+
+// const now = 2037;
+// const agePavan = now - 1994
+// const ageRashy = now - 1997
+// console.log(agePavan, ageRashy)
+
+// console.log(agePavan * 2, agePavan / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Pavan';
+// const lastName = 'Prakash';
+// console.log(firstName + ' ' + lastName);
+
+// // Assignment Opperators
+// let x = 10 + 5; //10 + 5 = 15
+// x += 10; // x = x + 10
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x) // returns 25
+
+
+// // Comparison Operators
+// console.log(agePavan > ageRashy); //true
+// console.log(ageRashy >= 18); //true
+
+// const isFullAge = ageRashy >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+// {
+//     [console.log(agePavan)]
+
+//     [console.log(ageRashy)]
+// }
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10 so both x and y are 10
+// console.log(x, y);
+
+// const averageAge = (agePavan + ageRashy) / 2
+// console.log(agePavan, ageRashy, averageAge);
+*/
+
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+*/
+
+
+// // MY ANSWER
+// var markWeight = 78
+// var markHeight = 1.69
+// var johnWeight = 92
+// var johnHeight = 1.95
+
+// // markWeight = 95 // comment out for data 1
+// // markHeight = 1.88 // comment out for data 1
+// // johnWeight = 85 // comment out for data 1
+// // johnHeight = 1.76 // comment out for data 1
+
+
+// console.log(markWeight / markHeight ** 2)
+// console.log(johnWeight / johnHeight ** 2)
+
+// let markBmi = markWeight / markHeight ** 2
+// let johnBmi = johnWeight / johnHeight ** 2
+
+
+// console.log('Does Mark have a higher BMI than John?')
+// console.log(markBmi > johnBmi) //false
+
+
+
